6c836a839a5396304b2fef62c2e84f75b331be7c
AflahPS <aflahps7@gmail.com>
2023-08-25 13:06:49 +0530

    Hero1 done

--- src/components/Heros/RightImagedHero/index.tsx
+++ src/components/Heros/RightImagedHero/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import { useStyles } from './styles';
-import Image from 'next/image';
 import printerSkeleton from '../../../assets/PrinterRightImage.png'
+import { CyanButton } from '@/shared';
 
 export const RightImagedHero = () => {
   const classes = useStyles()
@@ -11,9 +11,52 @@ export const RightImagedHero = () => {
       container
       className={classes.heroGridMain}
     >
-      <Grid flex={2} item>Text</Grid>
-      <Grid flex={1} item component={"image"} sx={{objectFit: "cover"}}>
-        <Image src={printerSkeleton} alt='printer-skeleton' height={700} />
+      <Grid
+        flex={3}
+        width={`75%`}
+        item
+        container
+        justifyContent={'center'}
+        direction={'column'}
+        gap={5}
+        ml={24}
+      >
+        <Grid item>
+          <Typography variant="h6">{`Small Header`}</Typography>
+        </Grid>
+
+        <Grid item>
+          <Typography variant="h2">
+            {`Lorem ipsum dolor sit amet consectetur, adipisicing elit.`}
+          </Typography>
+        </Grid>
+
+        <Grid item>
+          <Typography variant="h5">{`Something else`}</Typography>
+          <Typography variant="h5">{`Something more else`}</Typography>
+        </Grid>
+
+        <Grid item>
+        <CyanButton
+            buttonText='Book For Call'
+            handleClick={() => {}}
+          />
+        </Grid>
+      </Grid>
+      <Grid
+        flex={1}
+        width={`25%`}
+        item
+        height={'100%'}
+        overflow={'hidden'}
+      >
+        <Grid
+          component={'img'}
+          src={printerSkeleton.src}
+          sx={{ objectFit: 'cover' }}
+          height={'100%'}
+        />
+        {/* <Image src={printerSkeleton} alt='printer-skeleton' height={700} /> */}
       </Grid>
     </Grid>
   );
